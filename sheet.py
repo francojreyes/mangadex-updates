a7e813284995cbda67caccd14be4ef21437ef8f3
Franco Reyes <z5359122@ad.unsw.edu.au>
2022-03-27 16:07:41 +1100

    Script to read sheet for whitelist

---
+++ sheet.py
@@ -0,0 +1,32 @@
+'''
+Reads whitelisted manga ID's from google sheet
+'''
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+
+# defines
+SCOPE = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
+SHEET = 'Mangadex'
+
+# add credentials to the account
+creds = ServiceAccountCredentials.from_json_keyfile_name('credentials.json', SCOPE)
+
+# authorize the clientsheet 
+client = gspread.authorize(creds)
+
+def get_whitelist():
+    sheet = client.open(SHEET)
+    sheet_instance = sheet.get_worksheet(1)
+    return sheet_instance.col_values(2)
+
+
+def get_webhooks():
+    sheet = client.open(SHEET)
+    sheet_instance = sheet.get_worksheet(0)
+    return sheet_instance.col_values(1)
+
+
+if __name__ == '__main__':
+    # print(get_whitelist())
+    # print(get_webhooks())
+    pass
